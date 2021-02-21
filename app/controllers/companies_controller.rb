e0cff84a0655d771b90d2a7cc5ce55e69776f8b2
Matheus Oliveira <matheus.oliveirav@gmail.com>
2021-02-21 12:14:21 -0300

    Configurando o cadastro da empresa e o I18n

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,2 +1,28 @@
 class CompaniesController < ApplicationController
+  def index
+    @companies = Company.all
+  end
+
+  def show
+    @company = Company.find(params[:id])
+  end
+
+  def new
+    @companies = Company.new
+  end
+
+  def create
+    @company = Company.new(company_params)
+    #@company.user = current_user
+    if @company.save
+      redirect_to @company
+    else
+      render "new"
+    end
+  end
+
+  private
+    def company_params
+      params.require(:company).permit(:name, :address, :cnpj, :website, :founded, :logo)
+    end
 end
--- spec/features/create_a_company_spec.rb
+++ spec/features/create_a_company_spec.rb
@@ -2,6 +2,23 @@ require 'rails_helper'
 
 feature 'Admin creates a company' do
   scenario 'successfully' do
-    
+    # user = User.create!(email: 'matheus@code4u.com', password: '123456')
+
+    # COLOCAR A LOGICA DO LOGIN DO COLABORADOR
+
+    fill_in 'Nome', with: 'Code4U'
+    fill_in 'Endereço', with: 'Rua A 123'
+    fill_in 'CNPJ', with: '92107397000133'
+    fill_in 'Site', with: 'code4u.com'
+    fill_in 'Ano de Fundação', with: '2020'
+    attach_file 'Logo', Rails.root.join('spec', 'support', 'logo.png')
+    click_on 'Enviar'
+
+    expect(current_path).to eq company_path(Company.last)
+    expect(page).to have_content('Code4U')
+    expect(page).to have_content('Rua A 123')
+    expect(page).to have_content('92107397000133')
+    expect(page).to have_content('2020')
+    expect(page).to have_css('img[src*="logo.png"]')
   end
 end
